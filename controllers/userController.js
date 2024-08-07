95461e282b22d67fa528e39cb3ee1949da6ce87f
dominionbanjo <dominionbanjo@gmail.com>
2024-08-07 17:17:28 +0100

    fixed update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,8 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/userModel.js";
 import Job from "../models/jobModel.js";
 import cloudinary from "cloudinary";
-import { promises as fs } from "fs";
+// import { promises as fs } from "fs";
+import { formatImage } from "../middleware/multerMiddleware.js";
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId }).select("-password");
@@ -20,12 +21,14 @@ export const getApplicationStats = async (req, res) => {
 export const updateUser = async (req, res) => {
   const newUser = { ...req.body };
   delete newUser.password;
+
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path, {
+    const file = formatImage(req.file);
+    const response = await cloudinary.v2.uploader.upload(file, {
       use_filename: true,
       folder: "Images",
     });
-    await fs.unlink(req.file.path);
+
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
@@ -38,3 +41,25 @@ export const updateUser = async (req, res) => {
 
   res.status(StatusCodes.OK).json({ msg: "user updated" });
 };
+
+// export const updateUser = async (req, res) => {
+//   const newUser = { ...req.body };
+//   delete newUser.password;
+//   if (req.file) {
+//     const response = await cloudinary.v2.uploader.upload(req.file.path, {
+//       use_filename: true,
+//       folder: "Images",
+//     });
+//     await fs.unlink(req.file.path);
+//     newUser.avatar = response.secure_url;
+//     newUser.avatarPublicId = response.public_id;
+//   }
+
+//   const updatedUser = await User.findByIdAndUpdate(req.user.userId, newUser);
+
+//   if (req.file && updatedUser.avatarPublicId) {
+//     await cloudinary.v2.uploader.destroy(updatedUser.avatarPublicId);
+//   }
+
+//   res.status(StatusCodes.OK).json({ msg: "user updated" });
+// };
